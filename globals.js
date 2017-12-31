ffd31538df53bc038cab0a4c52c33b0ce3ac8540
Kassapo <ub@kassapo.com>
2017-12-31 12:02:30 +0100

    New German category: household items

--- globals.js
+++ globals.js
@@ -1,46 +1,51 @@
 "use strict";
-
 /*
  Copyright © 2017 Ulrich Becker, ub@kassapo.com
 
- This file is part of Ratespiel.
+ This file is part of Quiz.
 
- Ratespiel is free software: you can redistribute it and/or modify
+ Quiz is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- Ratespiel is distributed in the hope that it will be useful,
+ Quiz is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
 
  You should have received a copy of the GNU Affero General Public License
- along with Ratespiel.  If not, see <http://www.gnu.org/licenses/>.
+ along with Quiz.  If not, see <http://www.gnu.org/licenses/>.
 
  The source code is available at https://kassapo.github.com/quiz
  */
-
 const create = function (t) {
   return document.createElement(t)
 };
-
 function append(e) {
   return document.body.appendChild(e);
 }
-
 function insert(e) {
   return document.body.insertBefore(e, sources);
 }
-
 function remove(e) {
   document.body.removeChild(e);
 }
+function selectedIndices() {
+  return indices[selection.language][selection.topic];
+}
+function selectedTerms() {
+  return terms[selection.language][selection.topic];
+}
+function selectedTexts() {
+  return texts[selection.language];
+}
 
-const language = 'de';
 const indices = {};
+const selection = {};
 const terms = {};
 const texts = {};
+const topics = {};
 const easy = create('button');
 const done = create('button');
 const hard = create('button');
@@ -48,5 +53,6 @@ const next = create('button');
 const sources = create('div');
 const text = create('p');
 const title = create('h1');
+const topic = create('h5');
 const oneMinute = 60;
 const twoMinutes = 120;
